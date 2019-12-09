43a16dfc500d90a4247c8b1895b42bc7e0888b33
Ivan Petrushev <ivanatora@gmail.com>
2019-12-09 18:59:58 +0200

    Getting disk stats: free, used, total

--- snmp-qnap.sh
+++ snmp-qnap.sh
@@ -2,44 +2,38 @@
 HOST=192.168.2.250
 COMMUNITY=public
 
-disk=$(snmpget -v2c -c "$COMMUNITY" "$HOST" 1.3.6.1.4.1.24681.1.2.17.1.4.1 | awk '{print $4}' | sed 's/^"//')
-free=$(snmpget -v2c -c "$COMMUNITY" "$HOST" 1.3.6.1.4.1.24681.1.2.17.1.5.1 | awk '{print $4}' | sed 's/^"//')
-UNITtest=$(snmpget -v2c -c "$COMMUNITY" "$HOST" 1.3.6.1.4.1.24681.1.2.17.1.4.1 | awk '{print $5}' | sed 's/.*\(.B\).*/\1/')
-UNITtest2=$(snmpget -v2c -c "$COMMUNITY" "$HOST" 1.3.6.1.4.1.24681.1.2.17.1.5.1 | awk '{print $5}' | sed 's/.*\(.B\).*/\1/')
-    #echo $disk - $free - $UNITtest - $UNITtest2
+# disk total
+DISK_RAW=$(snmpget -v2c -c "$COMMUNITY" "$HOST" 1.3.6.1.4.1.24681.1.2.17.1.4.1)
+DISK_BYTES=$(echo $DISK_RAW | awk '{print $4}' | sed 's/"//')
+UNIT=$(echo $DISK_RAW | awk '{print $5}' | sed 's/"//')
 
-if [ "$UNITtest" == "TB" ]; then
- factor=$(echo "scale=0; 1000000" | bc -l)
-elif [ "$UNITtest" == "GB" ]; then
- factor=$(echo "scale=0; 1000" | bc -l)
-else
- factor=$(echo "scale=0; 1" | bc -l)
+#TODO: are we using 1000 or 1024 as scaling factor?
+if [ "$UNIT" == "GB" ]; then
+    DISK_BYTES_TOTAL=$(echo "$DISK_BYTES * 1000^3/1" | bc)
+fi
+if [ "$UNIT" == "TB" ]; then
+    DISK_BYTES_TOTAL=$(echo "$DISK_BYTES * 1000^4/1" | bc)
+fi
+if [ "$UNIT" == "PB" ]; then
+    DISK_BYTES_TOTAL=$(echo "$DISK_BYTES * 1000^5/1" | bc)
 fi
 
-if [ "$UNITtest2" == "TB" ]; then
- factor2=$(echo "scale=0; 1000000" | bc -l)
-elif [ "$UNITtest2" == "GB" ]; then
- factor2=$(echo "scale=0; 1000" | bc -l)
-else
- factor2=$(echo "scale=0; 1" | bc -l)
+# disk free
+DISK_RAW=$(snmpget -v2c -c "$COMMUNITY" "$HOST" 1.3.6.1.4.1.24681.1.2.17.1.5.1)
+DISK_BYTES=$(echo $DISK_RAW | awk '{print $4}' | sed 's/"//')
+UNIT=$(echo $DISK_RAW | awk '{print $5}' | sed 's/"//')
+
+#TODO: are we using 1000 or 1024 as scaling factor?
+if [ "$UNIT" == "GB" ]; then
+    DISK_BYTES_FREE=$(echo "$DISK_BYTES * 1000^3/1" | bc)
+fi
+if [ "$UNIT" == "TB" ]; then
+    DISK_BYTES_FREE=$(echo "$DISK_BYTES * 1000^4/1" | bc)
+fi
+if [ "$UNIT" == "PB" ]; then
+    DISK_BYTES_FREE=$(echo "$DISK_BYTES * 1000^5/1" | bc)
 fi
 
-#echo $factor - $factor2
-disk=$(echo "scale=0; $disk*$factor" | bc -l)
-free=$(echo "scale=0; $free*$factor2" | bc -l)
-
-#debug used=$(echo "scale=0; 9000*1000" | bc -l)
-used=$(echo "scale=0; $disk-$free" | bc -l)
-
-#echo $disk - $free - $used
-PERC=$(echo "scale=0; $used*100/$disk" | bc -l)
-
-diskF=$(echo "scale=0; $disk/$factor" | bc -l)
-freeF=$(echo "scale=0; $free/$factor2" | bc -l)
-usedF=$(echo "scale=0; $used/$factor" | bc -l)
-
-#wdisk=$(echo "scale=0; $strWarning*$disk/100" | bc -l)
-#cdisk=$(echo "scale=0; $strCritical*$disk/100" | bc -l)
-
-OUTPUT="Total:"$diskF"$UNITtest - Used:"$usedF"$UNITtest - Free:"$freeF"$UNITtest2 - Used Space: $PERC%|Used=$PERC;"
-echo $OUTPUT
+DISK_BYTES_USED=$(($DISK_BYTES_TOTAL - $DISK_BYTES_FREE))
+DISK_BYTES_USED_PERCENT=$(echo "$DISK_BYTES_USED * 100 / $DISK_BYTES_TOTAL" | bc)
+echo $DISK_BYTES_TOTAL $DISK_BYTES_USED $DISK_BYTES_USED_PERCENT
